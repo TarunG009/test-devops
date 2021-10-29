b7f5751cb4d41971fe8ff974ef1a4929e69e8c26
Tarun <tarun.garg@itsacheckmate.com>
2021-10-29 22:42:16 +0530

    small change

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   it "is valid with valid attributes"
-  it "is not valid without a title"
+  it "is not valid withoutasddd a title"
   it "is not valid without a description"
   it "is not valid without a start_date"
   it "is not valid without a end_date" do
