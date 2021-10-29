a87a602c768b77ff59ce0322824163e6e4ef72d5
Tarun <tarun.garg@itsacheckmate.com>
2021-10-29 22:38:28 +0530

    a test failure

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,5 +5,7 @@ RSpec.describe Product, type: :model do
   it "is not valid without a title"
   it "is not valid without a description"
   it "is not valid without a start_date"
-  it "is not valid without a end_date"
+  it "is not valid without a end_date" do
+    raise ActiveRecordNotFound
+  end
 end
